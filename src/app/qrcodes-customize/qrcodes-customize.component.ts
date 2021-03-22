1395780babb2a2ff27ab33dbd1ac75862e7dcc8e
Carlos <28845529+mesacarlos@users.noreply.github.com>
2021-03-22 11:46:41 +0100

    Customize selectors for dot type and size

--- src/app/qrcodes-customize/qrcodes-customize.component.ts
+++ src/app/qrcodes-customize/qrcodes-customize.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { QRCodeService } from '../core/service/qrcode.service';
+import { SessionService } from '../core/service/session.service';
+import { FormErrorStateMatcher } from '../core/util/FormErrorStateMatcher';
 
 @Component({
-  selector: 'app-qrcodes-customize',
-  templateUrl: './qrcodes-customize.component.html',
-  styleUrls: ['./qrcodes-customize.component.css']
+	selector: 'app-qrcodes-customize',
+	templateUrl: './qrcodes-customize.component.html',
+	styleUrls: ['./qrcodes-customize.component.css']
 })
 export class QrcodesCustomizeComponent implements OnInit {
+	matcher = new FormErrorStateMatcher();
+	sizeFormControl = new FormControl(256, [
+		Validators.required,
+		Validators.min(32),
+		Validators.max(2048),
+	]);
+	
+	dotStyles: NameValuePair[] = [
+		{nombre: 'Estándar', valor: 'square'},
+		{nombre: 'Puntos', valor: 'dot'},
+		{nombre: 'Redondeado', valor: 'round'},
+	];
 
-  constructor() { }
+	//Variables para los valores de los inputs
+	idQRCodeIdSet: boolean;
+	selectedDotStyle: NameValuePair = {nombre: 'Estándar', valor: 'square'};
+	selectedSize: number = 256;
 
-  ngOnInit(): void {
-  }
+	constructor(
+		private route: ActivatedRoute,
+		private sessService: SessionService,
+		private dialog: MatDialog,
+		private qrCodesService: QRCodeService,
+		private sanitizer: DomSanitizer,
+		private router: Router
+	) { }
 
+	ngOnInit(): void {
+		this.idQRCodeIdSet = this.route.snapshot.params.id != null;
+
+		if (!this.idQRCodeIdSet){
+			//Devolvemos al usuario a la lista de QRs ya que no hay QRCode id
+			this.router.navigate(['qrcodes']);
+		}
+
+		//Cargamos info del QR
+		this.generateQRCode();
+	}
+
+	generateQRCode() {
+
+	}
+
+}
+
+interface NameValuePair {
+	nombre: string;
+	valor: string;
 }
